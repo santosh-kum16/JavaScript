61311c4d1cfeb1c22b7556d67025370f28ccb9e2
santosh-kum16 <santosh.kum16@gmail.com>
2024-01-20 03:30:34 +0530

    Explanation about Data Types and Variable and suitable Examples for it.

---
+++ 1-JavaScript-BASICS/Variables_DataTypes.js
@@ -0,0 +1,47 @@
+
+//Variable is like a Container where we can Store values which uses in our code again and again.
+
+// Declaration of Variables:-
+
+// 1. STRING DATA TYPE -
+
+var fistName = 'Santosh';
+var lastName = 'Kumar';
+
+console.log(fistName);
+
+// Concat both values:-
+
+console.log(fistName+' '+lastName);
+
+//2. NUMBER DATA TYPE -
+
+var year = 2024;
+console.log(year);
+
+var kg = 115.234;
+console.log(kg);
+
+//3. BOOLEAN DATA TYPES - There are two values either 'true' or 'false'
+
+var thisIsMyAge = true;
+console.log(thisIsMyAge);
+
+//4. UNDEFINED DATA TYPE-
+
+// Just Declaration of variables
+var role;
+console.log(role); 
+
+/* So, above line Out put is 'Undefined'
+
+Now defining role var to some value
+*/
+
+role = 'Developer';
+
+console.log(role);
+
+// 5. NULL DATA TYPES- It means non-existent.
+
+// NOTE- camelCase is used to define variable name and must not use of Numbers and Special symbols excepts- Dollar symbol and Underscore.
