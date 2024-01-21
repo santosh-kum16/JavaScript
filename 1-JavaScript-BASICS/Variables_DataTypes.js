744ccd3fba75d1ed42dfcfd6d6ec3d63b0f9a169
santosh-kum16 <santosh.kum16@gmail.com>
2024-01-21 23:42:38 +0530

    typeof, let var and const added

--- 1-JavaScript-BASICS/Variables_DataTypes.js
+++ 1-JavaScript-BASICS/Variables_DataTypes.js
@@ -45,3 +45,41 @@ console.log(role);
 // 5. NULL DATA TYPES- It means non-existent.
 
 // NOTE- camelCase is used to define variable name and must not use of Numbers and Special symbols excepts- Dollar symbol and Underscore.
+
+//************************* */
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+/******************************* */
+// let, const and var
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Schmedtmann';
+console.log(lastName);
