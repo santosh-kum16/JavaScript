d0798a07c11204751b75f7249e3afae83d4794bf
Santosh Kumar <santosh.kum16@gmail.com>
2024-02-19 23:53:37 +0530

    Object accessing

--- JS-Concepts/Class.js
+++ JS-Concepts/Class.js
@@ -16,3 +16,16 @@ In the Person class, the constructor() is where you can initialize the propertie
 
 The following creates a new Person object, which will automatically call the constructor() of the Person class:
  */
+
+let santosh = new Person("Santosh Kumar");
+
+let name = santosh.getName();
+console.log(name); //Santosh Kumar
+
+// To verify the fact that classes are special functions, you can use the typeof operator of to check the type of the Person class.
+console.log(typeof Person); // function
+// It returns function as expected.
+
+//The santosh object is also an instance of the Person and Object types:-
+console.log(santosh instanceof Person); // true
+console.log(santosh instanceof Object); // true
